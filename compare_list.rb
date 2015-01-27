4fd26f2de3346c1530d4e1cdb0e2d3a08fd7147a
kenoboss <s3tozieg@web.de>
2015-01-27 09:26:10 +0100

    comments and lists
    new comments in the compare_list.rb - file and lists for cities and
    names

--- compare_list.rb
+++ compare_list.rb
@@ -20,22 +20,27 @@ word = satz.split(' ')
 #org = datei3.readlines
 #datei3.close
 
-
+# Testarrays
 loc = ["Aachen", "Berlin", "Trier"]
 per = ["Merkel", "Gauck", "Gabriel"]
 org = ["BASF", "Bayer", "Deutsches Rotes Kreuz"]
 
+#Indexierung für Eingabearray
 index = 0
 
+#Schreiben in externe .txt-Datei
 result = open("result.txt", "w")
 
 while index < word.length
+  #Vergleich "LOC-Lexikon" mit Eingabearray 
   if loc.include?(word[index])
     result.write(word[index] + "      LOC")
     result.write("\n")
+  #Vergleich "PER-Lexikon" mit Eingabearray 
   elsif per.include?(word[index])
     result.write(word[index] + "      PER")
     result.write("\n")
+  #Vergleich "ORG-Lexikon" mit Eingabearray   
   elsif org.include?(word[index])
     result.write(word[index] + "      ORG")
     result.write("\n")
