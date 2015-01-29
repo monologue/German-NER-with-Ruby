825ab46a83b85bbe0ecbc295e9239c28ad559eec
kenoboss <s3tozieg@web.de>
2015-01-29 14:21:46 +0100

    running compare list

--- compare_list.rb
+++ compare_list.rb
@@ -1,54 +1,22 @@
-# Eingabe des Textes oder des Satzes ohne Satzzeichen
-puts "Bitte geben Sie einen deutschen Satz oder Text ein:  "
-satz = gets
-# Zerlegung der Eingabe in Array durch Leerzeichentrennung
-word = []
-word = satz.split(' ')
+satz = "Maria ist mit Peter zu BASF gegangen, die in Trier sitzen. Danach ging Maria alleine zu Bayer die in Berlin sitzen."
 
-# Einlesen von Dateien mit Named Entities
+words = satz.split(' ')
 
-#datei1 = File.new("Staedte.txt", "r")
-#loc = []
-#loc = datei1.readlines
-#datei1.close
-#datei2 = File.new("Vornamen.txt", "r")
-#per = []
-#per = datei2.readlines
-#datei2.close
-#datei3 = File.new("Unternehmen.txt", "r")
-#org = []
-#org = datei3.readlines
-#datei3.close
+loc = File.readlines("Staedte.txt").map { |l| l.chomp  }
+per = File.readlines("Vornamen.txt").map { |l| l.chomp  }
+org = ["BASF", "Bayer"]
 
-# Testarrays
-loc = ["Aachen", "Berlin", "Trier"]
-per = ["Merkel", "Gauck", "Gabriel"]
-org = ["BASF", "Bayer", "Deutsches Rotes Kreuz"]
+index = 0 
 
-#Indexierung für Eingabearray
-index = 0
-
-#Schreiben in externe .txt-Datei
-result = open("result.txt", "w")
-
-while index < word.length
-  #Vergleich "LOC-Lexikon" mit Eingabearray 
-  if loc.include?(word[index])
-    result.write(word[index] + "      LOC")
-    result.write("\n")
-  #Vergleich "PER-Lexikon" mit Eingabearray 
-  elsif per.include?(word[index])
-    result.write(word[index] + "      PER")
-    result.write("\n")
-  #Vergleich "ORG-Lexikon" mit Eingabearray   
-  elsif org.include?(word[index])
-    result.write(word[index] + "      ORG")
-    result.write("\n")
+while index < words.length
+  if loc.include?(words[index])
+    puts words[index]+"   LOC"
+  elsif per.include?(words[index])
+    puts words[index]+"   PER"
+  elsif org.include?(words[index])
+    puts words[index]+"   ORG"
   else
-    result.write(word[index])
-    result.write("\n")
+    puts words[index]
   end
   index = index + 1
 end
-
-result.close
