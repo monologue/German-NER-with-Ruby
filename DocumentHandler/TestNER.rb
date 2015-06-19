233e6f6254849dd4badced7519b3c4819ee30728
monologue <reg.taube@gmail.com>
2015-06-19 16:15:09 +0200

    add test-class

---
+++ DocumentHandler/TestNER.rb
@@ -0,0 +1,27 @@
+require 'csv'
+
+class TestNER
+
+	attr_accessor :data1, :data2
+
+	def initialize
+		@data1 = "out.txt"
+		@data2 = "true.txt"
+	end
+	
+	def read_data(data1, data2)
+	end
+	
+	def true_positive
+		
+	end
+	
+	def false_positive
+	end
+	
+	def true_negative
+	end
+	
+	def false_negative
+	end
+end
