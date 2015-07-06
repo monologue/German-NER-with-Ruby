b15e13fcdb0c018c3bbb4aca565094699c0798fe
monologue <reg.taube@gmail.com>
2015-07-07 00:33:47 +0200

    small changes

--- DocumentHandler/TestNER.rb
+++ DocumentHandler/TestNER.rb
@@ -4,8 +4,8 @@ class TestNER
 
 	attr_accessor :data1, :data2, :count, :found, :wrong_found, :Columns, :not_ne
 	def initialize
-		@data1 = CSV.read("out.txt", {col_sep: "\t", quote_char: "\0", headers: true})
-		@data2 = CSV.read("train.txt", {col_sep: "\t", quote_char: "\0", headers: true})
+		@data1 = CSV.read("outm.txt", {col_sep: "\t", quote_char: "\0", headers: true})
+		@data2 = CSV.read("test.txt", {col_sep: "\t", quote_char: "\0", headers: true})
 		@count = { "PER" => 0,  "ORG" => 0, "LOC" => 0, "OTH" => 0}
 		@found = { 'PER' => 0,  "ORG" => 0, "LOC" => 0, "OTH" => 0}
 		@wrong_found = { "PER" => 0,  "ORG" => 0, "LOC" => 0, "OTH" => 0}
@@ -20,6 +20,11 @@ class TestNER
 	def compare(file1, file2)
 		same = true
 		for i in 0..file1.size-1
+		if file1[i]['Word'] =! file2[i]['Word']
+			puts file1[i]['Word']
+			puts i
+			exit
+		end
 			@Columns.each {|col|
 				if file1[i][col] != file2[i][col] && file2[i][col] == 'true' && file1[i][col] == 'false'
 					@count[col] += 1
